5fba1ea65d3285aa15c13d0078913f61ce7b9d59
RhysDeimel <deimelr@hotmail.com>
2021-03-15 16:30:00 +1100

    ignore, attributes, and utils frame

---
+++ tests/utils/cache.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Script for unpacking assets and creating a cache of resouces to test against
+
+Default behaviour is to unpack all resources on test run and leave them there.
+This might cause issues if assets are updated, so `clean` removes the cache.
+"""
+
+def create():
+	pass
+
+def clean():
+	pass
